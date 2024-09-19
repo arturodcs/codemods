fe115a0233cc6070a8f28119c3933a19ad19c178
Arturo Durán Castillo <arturo.duran@softtek.com>
2024-09-19 08:57:50 -0600

    refactor: extract functions from the main function

--- remove-unused-imports/index.ts
+++ remove-unused-imports/index.ts
@@ -8,79 +8,123 @@ import {
   JSCodeshift,
 } from "jscodeshift";
 
+function isUsedAsType(
+  j: JSCodeshift,
+  root: Collection<any>,
+  file: FileInfo,
+  varName: string
+) {
+  const isTypeScriptFile =
+    file.path.endsWith(".ts") || file.path.endsWith(".tsx");
+
+  if (!isTypeScriptFile) return false;
+  return (
+    root.find(j.TSTypeReference, { typeName: { name: varName } }).size() > 0
+  );
+}
+
+function isUsedInJSX(j: JSCodeshift, root: Collection<any>, varName: string) {
+  return root.find(j.JSXIdentifier, { name: varName }).size() > 0;
+}
+
 function shouldRemoveUnusedIdentifier(name: string) {
   return name !== "React";
+}
+
+function isUsedInScopes(
+  j: JSCodeshift,
+  root: Collection<any>,
+  importSpecifierPath: ASTPath<ImportSpecifier>,
+  varName: string
+) {
+  return (
+    root
+      .find(j.Identifier, { name: varName })
+      .filter((path) => {
+        // Ignorar el propio importador
+        if (
+          path.parentPath &&
+          path.parentPath.value === importSpecifierPath.value
+        ) {
+          return false;
+        }
+
+        // Excluir referencias en propiedades y claves de objetos
+        if (
+          path.name === "property" &&
+          (path.parentPath.value.type === "MemberExpression" ||
+            path.parentPath.value.type === "Property")
+        ) {
+          return false;
+        }
+
+        // Excluir referencias en declaraciones de importación
+        let currentPath = path;
+        while (currentPath) {
+          // @ts-ignore
+          if (currentPath.value.type === "ImportDeclaration") {
+            return false;
+          }
+          currentPath = currentPath.parentPath;
+        }
+        return true;
+      })
+      .size() > 0
+  );
+}
+
+function hasImportSpecifier(importDeclaration: ImportDeclaration): boolean {
+  return !!(
+    importDeclaration.specifiers && importDeclaration.specifiers.length > 0
+  );
+}
+
+function fixFirstNodeCommentsDeletion(
+  j: JSCodeshift,
+  root: Collection<any>,
+  originalFirstNode: any
+) {
+  // this function is used to fix the comments deletion issue when the first node is changed
+  // if there are changes in the first node and the original first node had comments
+  // then we need to add the comments to the new first node
+  const originalFirstNodeComments = originalFirstNode.comments;
+  const newFirstNodePath = root.find(j.Program).get("body", 0);
+  const newFirstNode = newFirstNodePath.node;
+
+  if (
+    originalFirstNode !== newFirstNode &&
+    originalFirstNodeComments &&
+    originalFirstNodeComments.length > 0
+  ) {
+    if (newFirstNode.comments && newFirstNode.comments.length > 0) {
+      newFirstNode.comments = [
+        ...originalFirstNodeComments,
+        ...newFirstNode.comments,
+      ];
+    } else {
+      newFirstNode.comments = originalFirstNodeComments;
+    }
+  }
 }
 
 export default function transformer(file: FileInfo, api: API, options) {
   const j: JSCodeshift = api.jscodeshift;
   const root: Collection<any> = j(file.source);
 
-  const isTypeScriptFile =
-    file.path.endsWith(".ts") || file.path.endsWith(".tsx");
+  const originalFirstNode = root.find(j.Program).get("body", 0).node;
 
-  const firstNode = root.find(j.Program).get("body", 0).node;
-  const firstNodeComments = firstNode.comments;
-
-  const removeIfUnused = (
-    importSpecifierPath: ASTPath<ImportSpecifier>,
-    importDeclarationPath: ASTPath<ImportDeclaration>
-  ) => {
+  const removeIfUnused = (importSpecifierPath: ASTPath<ImportSpecifier>) => {
     if (!importSpecifierPath.value.local) return;
     const varName = importSpecifierPath.value.local.name;
 
-    // Ignorar algunos identificadores
-    if (!shouldRemoveUnusedIdentifier(varName)) return;
-
-    const isUsedInScopes = () => {
-      return (
-        root
-          .find(j.Identifier, { name: varName })
-          .filter((path) => {
-            // Ignorar el propio importador
-            if (
-              path.parentPath &&
-              path.parentPath.value === importSpecifierPath.value
-            ) {
-              return false;
-            }
-
-            // Excluir referencias en propiedades y claves de objetos
-            if (
-              path.name === "property" &&
-              (path.parentPath.value.type === "MemberExpression" ||
-                path.parentPath.value.type === "Property")
-            ) {
-              return false;
-            }
-
-            // Excluir referencias en declaraciones de importación
-            let currentPath = path;
-            while (currentPath) {
-              // @ts-ignore
-              if (currentPath.value.type === "ImportDeclaration") {
-                return false;
-              }
-              currentPath = currentPath.parentPath;
-            }
-            return true;
-          })
-          .size() > 0
-      );
-    };
-
-    const isUsedInJSX = () => {
-      return root.find(j.JSXIdentifier, { name: varName }).size() > 0;
-    };
-
-    const isUsedAsType = () => {
-      if (!isTypeScriptFile) return false;
-      return (
-        root.find(j.TSTypeReference, { typeName: { name: varName } }).size() > 0
-      );
-    };
-
-    if (!(isUsedInScopes() || isUsedInJSX() || isUsedAsType())) {
+    if (
+      shouldRemoveUnusedIdentifier(varName) &&
+      !(
+        isUsedInScopes(j, root, importSpecifierPath, varName) ||
+        isUsedInJSX(j, root, varName) ||
+        isUsedAsType(j, root, file, varName)
+      )
+    ) {
       j(importSpecifierPath).remove();
       return true;
     }
@@ -93,36 +137,9 @@ export default function transformer(file: FileInfo, api: API, options) {
     const importSpecifiers = importDeclarationPath.get("specifiers");
 
     importSpecifiers.each((importSpecifierPath: ASTPath<ImportSpecifier>) => {
-      removeIfUnused(importSpecifierPath, importDeclarationPath);
+      removeIfUnused(importSpecifierPath);
     });
   };
-
-  function hasImportSpecifier(importDeclaration: ImportDeclaration): boolean {
-    return !!(
-      importDeclaration.specifiers && importDeclaration.specifiers.length > 0
-    );
-  }
-
-  // this function is used to fix the comments deletion issue when the first node is changed
-  function fixFirstNodeCommentsDeletion() {
-    // if there are changes in the first node and the original first node had comments
-    // then we need to add the comments to the new first node
-    if (
-      firstNode !== newFirstNode &&
-      firstNodeComments &&
-      firstNodeComments.length > 0
-    ) {
-      if (newFirstNode.comments && newFirstNode.comments.length > 0) {
-        newFirstNode.comments = [
-          ...firstNodeComments,
-          ...newFirstNode.comments,
-        ];
-      } else {
-        // 
-        newFirstNode.comments = firstNodeComments;
-      }
-    }
-  }
 
   root
     .find(j.ImportDeclaration)
@@ -141,10 +158,7 @@ export default function transformer(file: FileInfo, api: API, options) {
       }
     });
 
-  const newFirstNodePath = root.find(j.Program).get("body", 0);
-  const newFirstNode = newFirstNodePath.node;
-
-  fixFirstNodeCommentsDeletion();
+  fixFirstNodeCommentsDeletion(j, root, originalFirstNode);
 
   return root.toSource();
 }
